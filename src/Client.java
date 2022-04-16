9efb93594ce77a6b26d4098d70af70d475ee9a49
Natalya Shevchenko <nsevcenko1988@gmail.com>
2022-04-16 22:51:32 +0300

    error correction

--- src/Client.java
+++ src/Client.java
@@ -5,32 +5,23 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws IOException {
 
-        Socket serverClient = new Socket("127.0.0.1", 23489);
+        Socket сlient = new Socket("127.0.0.1", 23489);
 
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(serverClient.getInputStream()));
-             PrintWriter out = new PrintWriter(new OutputStreamWriter(serverClient.getOutputStream()), true);
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(сlient.getInputStream()));
+             PrintWriter out = new PrintWriter(new OutputStreamWriter(сlient.getOutputStream()), true);
              Scanner scanner = new Scanner(System.in)) {
 
             String message;
             System.out.println("Добрый день!");
             while (true) {
-                System.out.println("Введите число, которое хотите возвести в степень!");
+                System.out.println("Введите число и степерь в которую хотите возвести число, через запятую !");
                 message = scanner.nextLine();
                 if ("end".equals(message)) break;
                 out.println(message);
-                System.out.println("Введите степень в которую хотите возвести число");
-                String message2 = scanner.nextLine();
-                out.println(message2);
-                int result = pow(message, message2);
-                System.out.println("Результат: " + result);
 
+
+                System.out.println("SERVER: " + in.readLine());
             }
         }
-    }
-
-    public static int pow(String message, String message2) {
-        int result = (int) Math.pow(Integer.parseInt(message), Integer.parseInt(message2));
-        return result;
-
     }
 }
--- src/ComplexCalculations.java
+++ src/ComplexCalculations.java
@@ -15,17 +15,29 @@ public class ComplexCalculations {
                  BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
                 String line;
+                String [] massLine;
+
                 while ((line = in.readLine()) != null) {
+                    massLine = line.split(",");
+                    out.println(pow(massLine[0],massLine[1]));
+                }
 
                     if (line.equals("end")) {
                         break;
                     }
-                }
+
+
             } catch (IOException ex) {
                 ex.printStackTrace(System.out);
             }
         }
     }
+
+    public static String pow(String message, String message2) {
+        String result = Integer.toString((int) Math.pow(Integer.parseInt(message), Integer.parseInt(message2)));
+        return result;
+    }
+
 }
 // Был выбран Blocking вид, потому что важно чтобы поток был блокирован
 // при записи и потом при чтении, чтобы получить правильный ответ, чтобы поток не прервался и ответ был бы верным,
